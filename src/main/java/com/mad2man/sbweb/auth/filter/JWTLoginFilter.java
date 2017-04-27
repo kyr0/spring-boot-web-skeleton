e7b43d53af78a98ec837ee6d207f7af60a02f8ff
mad2Man <m.mansi1@gmx.net>
2017-04-27 04:08:35 +0200

    try to add auth

---
+++ src/main/java/com/mad2man/sbweb/auth/filter/JWTLoginFilter.java
@@ -0,0 +1,52 @@
+package com.mad2man.sbweb.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mad2man.sbweb.auth.service.TokenAuthenticationService;
+import com.mad2man.sbweb.auth.model.AccountCredentials;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Created by Mansi on 27.04.2017.
+ */
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public JWTLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+        AccountCredentials creds = new ObjectMapper()
+                .readValue(req.getInputStream(), AccountCredentials.class);
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        creds.getUsername(),
+                        creds.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest req,
+            HttpServletResponse res, FilterChain chain,
+            Authentication auth) throws IOException, ServletException {
+        TokenAuthenticationService
+                .addAuthentication(res, auth.getName());
+    }
+}
---
+++ src/main/java/com/mad2man/sbweb/auth/model/AccountCredentials.java
@@ -0,0 +1,26 @@
+package com.mad2man.sbweb.auth.model;
+
+/**
+ * Created by Mansi on 27.04.2017.
+ */
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
