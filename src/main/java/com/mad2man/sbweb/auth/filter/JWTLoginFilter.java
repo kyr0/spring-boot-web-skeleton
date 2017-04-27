7b0444e2a08f1fec4301be5ee22f1b21a2520c53
mad2Man <m.mansi1@gmx.net>
2017-04-28 01:25:08 +0200

    add jwt login filter missing password salt have to be included here

--- src/main/java/com/mad2man/sbweb/auth/filter/JWTLoginFilter.java
+++ src/main/java/com/mad2man/sbweb/auth/filter/JWTLoginFilter.java
@@ -1,9 +1,11 @@
 package com.mad2man.sbweb.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mad2man.sbweb.auth.service.TokenAuthenticationService;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.mad2man.sbweb.auth.model.AccountCredentials;
+import com.mad2man.sbweb.auth.service.TokenAuthenticationService;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -16,9 +18,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
 
-/**
- * Created by Mansi on 27.04.2017.
- */
 public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     public JWTLoginFilter(String url, AuthenticationManager authManager) {
@@ -28,25 +27,35 @@ public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Override
     public Authentication attemptAuthentication(
-            HttpServletRequest req, HttpServletResponse res)
-            throws IOException, ServletException {
-        AccountCredentials creds = new ObjectMapper()
-                .readValue(req.getInputStream(), AccountCredentials.class);
-        return getAuthenticationManager().authenticate(
+        HttpServletRequest req, HttpServletResponse res)
+        throws IOException, ServletException {
+        try {
+            AccountCredentials creds = new ObjectMapper()
+                .readValue(req.getReader(), AccountCredentials.class);
+
+            if ((creds.getUsername() == null || creds.getPassword() == null)) {
+                throw new AuthenticationServiceException("Username or Password not provided");
+            }
+            return getAuthenticationManager().authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        creds.getUsername(),
-                        creds.getPassword(),
-                        Collections.emptyList()
+                    creds.getUsername(),
+                    //TODO: get here salt from db and add it here!!! to password
+                    creds.getPassword(),
+                    Collections.emptyList()
                 )
-        );
+            );
+        } catch (UnrecognizedPropertyException e) {
+            throw new AuthenticationServiceException("Missing (2 known properties: \"password\", \"username\"])");
+
+        }
     }
 
     @Override
     protected void successfulAuthentication(
-            HttpServletRequest req,
-            HttpServletResponse res, FilterChain chain,
-            Authentication auth) throws IOException, ServletException {
+        HttpServletRequest req,
+        HttpServletResponse res, FilterChain chain,
+        Authentication auth) throws IOException, ServletException {
         TokenAuthenticationService
-                .addAuthentication(res, auth.getName());
+            .addAuthentication(res, auth.getName());
     }
 }
